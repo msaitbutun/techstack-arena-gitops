c4176d37fac1e3e72b8f98a04d004633f565b991
msaitbutun <[msait.butun@ogr.ksbu.edu.tr]>
2025-11-26 12:53:16 +0300

    yamllar eklendi

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+resource "google_container_cluster" "primary"{
+    name = "my-gitops-cluster"
+    location = "europe-west1-b"
+    remove_default_node_pool = true
+    initial_node_count = 1 
+}
+
+resource "google_container_node_pool" "primary_nodes"{
+    name = "sait-node-pool"
+    location = "europe-west1-b"
+    cluster = google_container_cluster.primary.name
+    node_count = 2
+    node_config {
+    preemptible  = true # Spot instance benzeri (Çok daha ucuzdur, demo için ideal)
+    machine_type = "e2-medium" # 2 vCPU, 4GB RAM (Rahat rahat yeter)
+
+    # Google servislerine erişim yetkileri
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+   
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "elegant-verbena-475815-g8"
+region     = "europe-west1"
+zone       = "europe-west1-b"
